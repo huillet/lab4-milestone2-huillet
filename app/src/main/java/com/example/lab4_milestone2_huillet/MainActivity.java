fba05f747b3cc46eeb13816312063b469c2acc3e
Levi Huillet <levimhuillet@gmail.com>
2021-10-07 23:27:04 -0500

    checkpoint M2-8

--- app/src/main/java/com/example/lab4_milestone2_huillet/MainActivity.java
+++ app/src/main/java/com/example/lab4_milestone2_huillet/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.lab4_milestone2_huillet;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int LOCATION_REQUEST_LOCATION = 50;
 
     LocationManager locationManager;
     LocationListener locationListener;
@@ -43,5 +50,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+
+        if (Build.VERSION.SDK_INT < 23) {
+            startListening();
+        }
+        else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)  == PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_LOCATION);
+            }
+            else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null)
+                {
+                    updateLocationInfo(location);
+                }
+            }
+        }
+    }
+
+    private void updateLocationInfo(Location location) {
+
+    }
+
+    public void startListening() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
     }
 }
